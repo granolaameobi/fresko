b2a39bc80f9ac04d3c9fb918b4eb97dd8592a398
Ian Russell <ianchristopher.russell@googlemail.com>
2023-07-07 13:04:26 +0100

    test: add tests

--- tests/test_boot.py
+++ tests/test_boot.py
@@ -1,7 +1,29 @@
 from app import app
 import pytest
 
-def test_app_runs_without_errors():
-    with app.test_client() as client:
-        response = client.get('/')
-        assert response.status_code == 200
+@pytest.fixture
+def client(scope="session"):
+    with app.test_client() as test_client:
+        yield test_client
+
+def test_app_runs_without_errors(client):
+    response = client.get('/')
+    assert response.status_code == 200
+
+def test_menu(client):
+    response = client.get('/menu')
+    assert response.status_code == 200
+
+def test_reservation_form(client):
+    response = client.get('/reservation')
+    assert response.status_code == 200
+
+def test_reservation_submission(client):
+    data = {
+        'name': 'John Smith',
+        'date': '2022-01-01',
+        'time': '18:00',
+        'party_size': '4'
+    }
+    response = client.post('/reservation', data=data)
+    assert response.status_code == 200
