3784ce0be8c22f35c7dd5b8f938688ba53725c8f
Ian Russell <ianchristopher.russell@googlemail.com>
2023-07-12 13:39:52 +0100

    feat: update reservations

--- app.py
+++ app.py
@@ -34,12 +34,17 @@ def dinner():
 def reservation():
     if request.method == 'POST':
         # Handle form submission
-        name = request.form['name']
+        first_name = request.form['first_name']
+        last_name = request.form['last_name']
+        email = request.form['email']
+        contact_number = request.form['contact_number']
         date = request.form['date']
         time = request.form['time']
         party_size = request.form['party_size']
         # Save reservation to a database or file
-        return render_template('reservation_confirmation.html', name=name, date=date, time=time, party_size=party_size)
+        return render_template('reservation_confirmation.html', first_name=first_name, last_name=last_name,
+                               email=email,contact_number=contact_number,date=date,
+                               time=time, party_size=party_size)
     else:
         # Render the reservation form
         return render_template('reservation_form.html')
