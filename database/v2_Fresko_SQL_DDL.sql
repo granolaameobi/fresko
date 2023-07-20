7865c64e6c8cf01acdd50d5ec90b90c158ea8e9e
NickyB9 <97201187+NickyB9@users.noreply.github.com>
2023-07-20 16:38:54 +0100

    Update v2_Fresko_SQL_DDL.sql

--- database/v2_Fresko_SQL_DDL.sql
+++ database/v2_Fresko_SQL_DDL.sql
@@ -20,8 +20,8 @@ $$ LANGUAGE plpgsql;
 
 SELECT drop_all_tables();
 */
---Create Employee table
-CREATE TABLE IF NOT EXISTS "Employee" (
+--Create employee table
+CREATE TABLE IF NOT EXISTS "employee" (
   employee_id SERIAL PRIMARY KEY,
   first_name TEXT,
   last_name TEXT,
@@ -34,25 +34,25 @@ CREATE TABLE IF NOT EXISTS "Employee" (
   employee_gender TEXT
 );
 
--- Create the Table_number table
-CREATE TABLE IF NOT EXISTS "Table_number" (
+-- Create the table_number table
+CREATE TABLE IF NOT EXISTS "table_number" (
   table_id SERIAL PRIMARY KEY,
   capacity INT
 );
 
--- Create the Order table
-CREATE TABLE IF NOT EXISTS "Order" (
+-- Create the order table
+CREATE TABLE IF NOT EXISTS "order" (
   order_id SERIAL PRIMARY KEY,
   time_ordered TIMESTAMP,
   time_delivered TIMESTAMP,
   table_id INT,
   employee_id INT,
-  FOREIGN KEY (table_id) REFERENCES "Table_number" (table_id),
-  FOREIGN KEY (employee_id) REFERENCES "Employee" (employee_id)
+  FOREIGN KEY (table_id) REFERENCES "table_number" (table_id),
+  FOREIGN KEY (employee_id) REFERENCES "employee" (employee_id)
 );
 
--- Create the Booking table
-CREATE TABLE "Booking" (
+-- Create the booking table
+CREATE TABLE "booking" (
   booking_id SERIAL PRIMARY KEY,
   booking_name TEXT,
   group_size INT,
@@ -61,64 +61,61 @@ CREATE TABLE "Booking" (
   start_time TIMESTAMP,
   duration INTERVAL DEFAULT ('1.5 hours'), --Cannot do end_time as cannot reference start time for default
   table_id INT,
-  FOREIGN KEY (table_id) REFERENCES "Table_number" (table_id)
+  FOREIGN KEY (table_id) REFERENCES "table_number" (table_id)
 );
 
---Create Payment table
-CREATE TABLE IF NOT EXISTS "Payment" (
+--Create payment table
+CREATE TABLE IF NOT EXISTS "payment" (
   payment_id SERIAL PRIMARY KEY,
   order_id INT,
   payment_method TEXT,
   amount MONEY,
   payment_time TIMESTAMP,
-  FOREIGN KEY (order_id)  REFERENCES "Order" (order_id)
+  FOREIGN KEY (order_id)  REFERENCES "order" (order_id)
 );
 
-
-
---Create Payroll table
-CREATE TABLE IF NOT EXISTS "Payroll" (
+--Create payroll table
+CREATE TABLE IF NOT EXISTS "payroll" (
   salary_id SERIAL PRIMARY KEY,
   employee_id INT,
   year INT,
   month VARCHAR(10),
   hours_worked FLOAT,
   hourly_rate FLOAT,
-  FOREIGN KEY (employee_id)  REFERENCES "Employee" (employee_id)
+  FOREIGN KEY (employee_id)  REFERENCES "employee" (employee_id)
 );
 
--- Create the Menu_item table
-CREATE TABLE IF NOT EXISTS "Menu_item" (
+-- Create the menu_item table
+CREATE TABLE IF NOT EXISTS "menu_item" (
   menu_item_id SERIAL PRIMARY KEY,
   menu_item_name TEXT,
-  course VARCHAR(10),
   price MONEY,
-  available VARCHAR(10) 
+  category TEXT
 );
 
--- Create the Order_item table
-CREATE TABLE IF NOT EXISTS "Order_item" (
+-- Create the order_item table
+CREATE TABLE IF NOT EXISTS "order_item" (
   order_id INT,
   menu_item_id INT,
   quantity INT,
   PRIMARY KEY (order_id, menu_item_id),
-  FOREIGN KEY (order_id) REFERENCES "Order" (order_id),
-  FOREIGN KEY (menu_item_id) REFERENCES "Menu_item" (menu_item_id)
+  FOREIGN KEY (order_id) REFERENCES "order" (order_id),
+  FOREIGN KEY (menu_item_id) REFERENCES "menu_item" (menu_item_id)
 );
 
 
-/*-- Create link table between Booking and TableNumber
+/*-- Create link table between booking and table_number
 CREATE TABLE "BookingLinkTableNumber" (
   booking_id INT,
   table_id INT,
   PRIMARY KEY (booking_id, table_id),
-  FOREIGN KEY (booking_id) REFERENCES "Booking" (booking_id),
-  FOREIGN KEY (table_id) REFERENCES "TableNumber" (table_id)
+  FOREIGN KEY (booking_id) REFERENCES "booking" (booking_id),
+  FOREIGN KEY (table_id) REFERENCES "table_number" (table_id)
 );
 */ -- Probably not needed
 
---Create Supplier table
-CREATE TABLE IF NOT EXISTS "Supplier" (
+--Create supplier table
+CREATE TABLE IF NOT EXISTS "supplier" (
   supplier_id SERIAL PRIMARY KEY,
   supplier_name TEXT,
   supplier_address TEXT,
@@ -126,58 +123,60 @@ CREATE TABLE IF NOT EXISTS "Supplier" (
   supplier_email TEXT
 );
 
--- Creat Ingredients table
-CREATE TABLE IF NOT EXISTS "Ingredient" (
+-- Create ingredient table
+CREATE TABLE IF NOT EXISTS "ingredient" (
   ingredient_id SERIAL PRIMARY KEY,
   ingredient_name TEXT,
+  supplier_quantity TEXT
   low_threshold INT,
   unit TEXT,
+  low_threshold_grams INT,
   supplier_id INT,
-  FOREIGN KEY (supplier_id) REFERENCES "Supplier" (supplier_id)
+  FOREIGN KEY (supplier_id) REFERENCES "supplier" (supplier_id)
 );
 
---Create Recipe table
-CREATE TABLE "Recipe" (
+--Create recipe table
+CREATE TABLE "recipe" (
   menu_item_id INT,
   ingredient_id INT,
   quantity NUMERIC,
   PRIMARY KEY (menu_item_id, ingredient_id),
-  FOREIGN KEY (menu_item_id) REFERENCES "Menu_item" (menu_item_id),
-  FOREIGN KEY (ingredient_id) REFERENCES "Ingredient" (ingredient_id)
+  FOREIGN KEY (menu_item_id) REFERENCES "menu_item" (menu_item_id),
+  FOREIGN KEY (ingredient_id) REFERENCES "ingredient" (ingredient_id)
 );
 
--- Create Allergens table
-CREATE TABLE IF NOT EXISTS "Allergen" (
+-- Create allergen table
+CREATE TABLE IF NOT EXISTS "allergen" (
   allergen_id SERIAL PRIMARY KEY,
   allergen_name TEXT
 );
 
--- Create Ingredient_allergens table
-CREATE TABLE "Ingredient_allergen" (
+-- Create ingredient_allergen table
+CREATE TABLE "ingredient_allergen" (
   ingredient_id INT,
   allergen_id INT,
   PRIMARY KEY (ingredient_id, allergen_id),
-  FOREIGN KEY (ingredient_id) REFERENCES "Ingredient" (ingredient_id),
-  FOREIGN KEY (allergen_id) REFERENCES "Allergen" (allergen_id)
+  FOREIGN KEY (ingredient_id) REFERENCES "ingredient" (ingredient_id),
+  FOREIGN KEY (allergen_id) REFERENCES "allergen" (allergen_id)
 );
 
---Create (storage) Locations table 
-CREATE TABLE IF NOT EXISTS "Location" (
+--Create (storage) location table 
+CREATE TABLE IF NOT EXISTS "location" (
   location_id SERIAL PRIMARY KEY,
   location_name TEXT
 ); 
 
--- Create Current Stock table
-CREATE TABLE IF NOT EXISTS "Current_stock" (
+-- Create current_stock table
+CREATE TABLE IF NOT EXISTS "current_stock" (
   ingredient_id INT,
   location_id INT,
-  expiry_date DATE,
-  total_cost MONEY,
   delivery_date DATE,
+  total_cost MONEY,
+  expiry_date DATE,
   quantity NUMERIC,
   PRIMARY KEY (ingredient_id, expiry_date, location_id, total_cost),
-  FOREIGN KEY (ingredient_id) REFERENCES "Ingredient" (ingredient_id),
-  FOREIGN KEY (location_id) REFERENCES "Location" (location_id)
+  FOREIGN KEY (ingredient_id) REFERENCES "ingredient" (ingredient_id),
+  FOREIGN KEY (location_id) REFERENCES "location" (location_id)
 );
 
 
