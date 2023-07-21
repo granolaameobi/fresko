293222a1afc4158346d41281ad02039776e84c6e
Ian Russell <ianchristopher.russell@googlemail.com>
2023-07-21 14:41:15 +0100

    refactor: remove redunant code

--- app.py
+++ app.py
@@ -27,13 +27,7 @@ def lunch():
 
 @app.route('/dinner')
 def dinner():
-    # Retrieve menu items from a database or file
-    menu_items = [
-        {'name': 'Pork (GF)', 'price': '9.00'},
-        {'name': 'Lamb (GF)', 'price': '9.00'},
-        {'name': 'Falafel (GF)', 'price': '9.50'}
-    ]
-    return render_template('dinner.html',menu_items=menu_items)
+    return render_template('dinner.html')
 
 @app.route('/reservation', methods=['GET', 'POST'])
 def reservation():
